1ee7c958cd9ae48e05a233befb0186a58375df9b
acessiane <acessiane@gmail.com>
2023-09-26 21:24:23 +0900

    Level

--- Assets/Scenes/W07/TestBake.cs
+++ Assets/Scenes/W07/TestBake.cs
@@ -16,6 +16,6 @@ public class TestBake : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        surface.BuildNavMesh();
+        surface.BuildNavMeshAsync();
     }
 }
